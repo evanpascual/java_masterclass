83115bbfb275eb399790a4d10d2dcce5d1f117c4
Evan Pascual <evanpascual@evans-imac.home>
2021-02-10 09:20:36 -0800

    added helloWorld file

---
+++ HelloWorld/src/App.java
@@ -0,0 +1,10 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+     }
+}
